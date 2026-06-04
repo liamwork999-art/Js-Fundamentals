2c3b5cfc525b170baac6ac10e44b26e424ef018e
Liamxoom <liamwork999@gmail.com>
2026-06-04 21:04:01 +0300

    fisrt JavaScript script

---
+++ section-01_js-fundamentals/script.js
@@ -0,0 +1,21 @@
+// This is Part-01 JavaScript fundamentals
+
+// The first line of code
+
+/*
+
+console.log("Hello Wrold!");
+*/
+
+// **Values and Variables**
+
+console.log("Liam"); // now Liam is a value
+console.log(23); // the number is a value too
+// a value is the smallest data we have in Js
+
+let firstName = 'Liam';
+console.log(firstName);
+
+// 'firstName' is called a variable and they store a value in them
+
+// Naming rules and conventions camelCase.
