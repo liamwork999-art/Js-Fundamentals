5d2f4b3b8e4dbf3578b308116c1642e09ffee550
Liam_Araya <liamwork999@gmail.com>
2026-06-06 20:50:56 +0300

    started section-02_js-fundamntals/script.js

--- section-02_js-fundamentals/script.js
+++ section-02_js-fundamentals/script.js
@@ -5,5 +5,27 @@
     You can uncomment the code to see the output and understand the concepts better.
 */
 
-'use strict';
-// 🎯
+'use strict'; // This will strict our coding
+
+//  🎯Function
+
+function logger() {
+    console.log('My name is liam');
+}
+
+logger(); // calling / running / invoking functions
+
+function fruitP(apples, mangos) { // This one way of assigning a function
+    // console.log(apples, mangos);
+    const juice = `Juice with ${apples} apples and ${mangos} mangos.`;
+    return juice; // This returns the value of juice.
+}
+
+const juice = fruitP(2, 3); // This returns the value of juice.
+console.log(juice);
+
+
+const appleMangoJuice = fruitP(2, 4);
+console.log(appleMangoJuice);
+
+    
