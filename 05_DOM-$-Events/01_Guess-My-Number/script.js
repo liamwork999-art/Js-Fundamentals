f7b9b21649d01a3c73fb8502f0c77656a709a335
Liam_Araya <liamwork999@gmail.com>
2026-07-04 09:23:04 +0300

    Delete 05_DOM-$-Events/01_Guess-My-Number directory

--- 05_DOM-$-Events/01_Guess-My-Number/script.js
+++
@@ -1,7 +0,0 @@
-// Guess My Number
-
-'use strict';
-
-console.log(document.querySelector('.message').textContent);
-console.log('hi');
-// Post-ponned Lesson -- coming back when Html and CSS are done
