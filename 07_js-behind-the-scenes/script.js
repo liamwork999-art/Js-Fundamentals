f9ca93ca53f27e37f6ce66fdf478d3c9a71d4816
Liam_Araya <liamwork999@gmail.com>
2026-08-18 15:37:53 +0300

    Variable Hoisting

---
+++ 07_js-behind-the-scenes/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Defined in a Global scope
+function calcAge(birthyear) {
+  const age = 2037 - birthyear;
+
+  function printAge() {
+    const outPut = `You are ${age}, born in ${birthyear}`;
+    console.log(outPut);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'jonas';
+calcAge(1991);
